5b38c1fb7e58d24d637427fb452a54b79378542a
Billy.Zheng <zw963@163.com>
2015-01-05 13:33:02 +0800

    fix csv filename created

--- lib/common.rb
+++ lib/common.rb
@@ -140,7 +140,7 @@ module Common
   def keyword_csv_filename
     fail '不存在抓取关键字!' if $keyword.nil?
 
-    keyword_csv_filename = "#{site_directory}/#{$keyword}.csv"
+    keyword_csv_filename = "#{site_directory}/#{$keyword.tr('/', "\uff0f")}.csv"
 
     if test 's', keyword_csv_filename
       logger_with_puts "\033[0;33m#{keyword_csv_filename}\033[0m 文件存在, 跳过 !"
--- lib/keyword_logger.rb
+++ lib/keyword_logger.rb
@@ -8,7 +8,7 @@ class KeywordLogger
     logger.progname = "#{tags.join}"
     logger.info "\n\n" + '*'*100 + "\n启动 #{log_name}.\n" + '*'*100 + "\n"
 
-    logger.info "开始抓取 #{keyword_output} #{category}#{task}"
+    logger.info "开始抓取 #{keyword_output} #{category}#{task}."
     puts "开始抓取 #{keyword_output} #{category}#{task}."
 
     logger
