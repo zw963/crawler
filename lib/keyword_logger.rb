00d8df3264e4337d92866a5adda901d07bac2af0
Billy.Zheng <zw963@163.com>
2015-01-15 11:58:46 +0800

    fix create_json

--- lib/keyword_logger.rb
+++ lib/keyword_logger.rb
@@ -9,7 +9,6 @@ class KeywordLogger
     logger.info "\n\n" + '*'*100 + "\n启动 #{log_name}.\n" + '*'*100 + "\n"
 
     logger.info "开始抓取 #{keyword_output} #{category}#{task}."
-    puts "开始抓取 #{keyword_output} #{category}#{task}."
 
     logger
   end
